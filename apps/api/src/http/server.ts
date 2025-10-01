78ecbc67366c8c449aba84051039c20904e99f0b
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-01 15:09:19 +0200

    feat: configurando tabelas e executando migracoes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { createAccount } from "./routes/auth/create-account";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -8,6 +9,8 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
+
+app.register(createAccount)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
