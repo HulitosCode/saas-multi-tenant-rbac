2c15d3e450f23fc27b08f8063d03db08984d9f24
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-01 13:58:38 +0200

    feat: configurando fastify

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running')
+})
