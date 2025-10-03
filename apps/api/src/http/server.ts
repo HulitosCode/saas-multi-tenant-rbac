72747af865b47302714cd69139c9285e64e17cae
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-03 12:12:21 +0200

    feat: criando rota para atualizar irganizacao

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,6 +21,7 @@ import { createOrganization } from "./routes/orgs/create-organization";
 import { getMembership } from "./routes/orgs/get-membership";
 import { getOrganizations } from "./routes/orgs/get-organizations";
 import { getOrganization } from "./routes/orgs/get-organization";
+import { updateOrganization } from "./routes/orgs/update-organization";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -73,6 +74,7 @@ app.register(getMembership)
 
 app.register(getOrganization)
 app.register(getOrganizations)
+app.register(updateOrganization)
 
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
