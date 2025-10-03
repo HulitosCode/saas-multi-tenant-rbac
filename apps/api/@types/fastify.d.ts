5862b14446a6bfe007f76aa3e5c05e80cb27fb5c
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-03 10:59:57 +0200

    feat: obtendo afiliacao

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,9 @@
 import "fastify";
+import { Organization } from "generated/prisma";
 
 declare module "fastify" {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>;
+    getUserMembership(slug: string): Promise<{ organization: Organization, membership }>;
   }
 }
