5bc80c742c6e31d9d72b17b1dbcc150b3f532295
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-03 15:59:19 +0200

    feat: criando rotas de update e delete dos membos

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -30,6 +30,8 @@ import { getProject } from "./routes/projects/get-project";
 import { getProjects } from "./routes/projects/get-projects";
 import { updateProject } from "./routes/projects/update-project";
 import { getMembers } from "./routes/members/get-member";
+import { updateMembers } from "./routes/members/update-members";
+import { removeMember } from "./routes/members/remove-member";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -93,6 +95,9 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMembers)
+app.register(removeMember)
+
 
 
 
